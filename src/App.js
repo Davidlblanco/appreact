16d3ca129980faa9ca384033ead147ca703de42d
David Lucas Blanco <david.blanco@maeztra.com>
2021-04-17 19:42:51 -0300

    luana mario

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
+const luana = {
+    cliente: 'Luana',
+    idade: 27,
+    compras: [
+        { nome: 'Notebook', preco: 'R$ 2500' },
+        { nome: 'Geladeira', preco: 'R$ 3000' },
+        { nome: 'Smartphone', preco: 'R$ 1500' },
+    ],
+    ativa: true,
+};
+
+const mario = {
+    cliente: 'Mario',
+    idade: 31,
+    compras: [
+        { nome: 'Notebook', preco: 'R$ 2500' },
+        { nome: 'Geladeira', preco: 'R$ 3000' },
+        { nome: 'Smartphone', preco: 'R$ 1500' },
+        { nome: 'Guitarra', preco: 'R$ 3500' },
+    ],
+    ativa: false,
+};
+
+let ativa = (dados) => {
+    return dados.ativa ? 'Ativa' : 'Inativa';
+}
+
+let total = (dados) => {
+    let total = 0;
+    dados.compras.forEach(element => {
+        total = total + parseInt(element.preco.split('R$ ')[1])
+    });
+    return total;
+}
 
 function App() {
-  // > 1500 + lista de cores
-  return (
-    <div>
-      {
-        produtos.map(item => {
-          return (
-            Number(item.preco.replace('R$ ', '')) > 1500 &&
-            <div key={item.id}>
-              <h1>{item.nome}</h1>
-              <p>Preço: {item.preço}</p>
-              <ul>{
-                item.cores.map(color => {
-                  return <li style={{ background: color, color: '#fff' }}> {color}</li>
-                })
-              }</ul>
-            </div>)
-        })
-      }
-    </div >
-  );
+    const dados = luana;
+    return (
+        <div className="App">
+            <p>Nome: {dados.cliente}</p>
+            <p>Idade: {dados.idade}</p>
+            <p className={ativa(dados).toLocaleLowerCase()}>Situação: <span>{ativa(dados)}</span></p>
+            <p>Total gasto: {'R$ ' + total(dados)}</p>
+            {total(dados) > 10000 && <p>  "Voce está gastando muito"</p>}
+        </div>
+    );
 }
 
 export default App;
