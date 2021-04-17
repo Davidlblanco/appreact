e91489656ae6288cdd77129aa53473afe6d83dda
David Lucas Blanco <david.blanco@maeztra.com>
2021-04-17 19:27:42 -0300

    colors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 function App() {
+  // > 1500 + lista de cores
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {
+        produtos.map(item => {
+          return (
+            Number(item.preco.replace('R$ ', '')) > 1500 &&
+            <div key={item.id}>
+              <h1>{item.nome}</h1>
+              <p>Preço: {item.preço}</p>
+              <ul>{
+                item.cores.map(color => {
+                  return <li style={{ background: color, color: '#fff' }}> {color}</li>
+                })
+              }</ul>
+            </div>)
+        })
+      }
+    </div >
   );
 }
 
