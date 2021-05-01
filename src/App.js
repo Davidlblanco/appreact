c93daf861926602f820ef050e6569f726b93432c
David Lucas Blanco <david.blanco@maeztra.com>
2021-05-01 15:55:32 -0300

    slide/pic

--- src/App.js
+++ src/App.js
@@ -1,78 +1,21 @@
 //rfc
-import React, { useState } from 'react'
+import React from 'react'
+import slide1 from './img/slide1.jpeg'
+import slide2 from './img/slide2.jpeg'
+import slide3 from './img/slide3.jpeg'
+import Slide from './Slide'
 
-const perguntas = [
-  {
-    pergunta: 'Qual método é utilizado para criar componentes?',
-    options: [
-      'React.makeComponent()',
-      'React.createComponent()',
-      'React.createElement()',
-    ],
-    resposta: 'React.createElement()',
-    id: 'p1',
-  },
-  {
-    pergunta: 'Como importamos um componente externo?',
-    options: [
-      'import Component from "./Component"',
-      'require("./Component")',
-      'import "./Component"',
-    ],
-    resposta: 'import Component from "./Component"',
-    id: 'p2',
-  },
-  {
-    pergunta: 'Qual hook não é nativo?',
-    options: ['useEffect()', 'useFetch()', 'useCallback()'],
-    resposta: 'useFetch()',
-    id: 'p3',
-  },
-  {
-    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
-    options: ['set', 'get', 'use'],
-    resposta: 'use',
-    id: 'p4',
-  },
-];
 function App() {
-  const [stage, setStage] = useState(0);
-  const [answer, setAnswer] = useState({});
-
-  function checkResult(answer) {
-    let correct = 0;
-    perguntas.forEach((item, index) => {
-      if (item.resposta === answer[index]) {
-        correct++
-      }
-    })
-    return correct
-  }
-
+  const slides = [
+    { id: 'slide1', text: 'slide1', picture: slide1 },
+    { id: 'slide2', text: 'slide2', picture: slide2 },
+    { id: 'slide3', text: 'slide3', picture: slide3 },
+  ]
   return (
-    stage < 4 ?
-      <React.Fragment>
-        <h1>{perguntas[stage].pergunta}</h1>
-        {perguntas[stage].options.map((option, index) => {
-          return (
-            <label key={index}>
-              <input type={'radio'}
-                name={perguntas[stage].pergunta}
-                value={option}
-                checked={answer[stage] === option ? true : false}
-                onChange={({ target }) => { setAnswer({ ...answer, [stage]: option }) }} />
-              {option}
-            </label>
-          )
-        })}
-        <button onClick={() => { const newStage = stage + 1; setStage(newStage) }} disabled={!answer[stage]} >Próximo </button>
-      </React.Fragment>
-      : <React.Fragment>Resultado final: {checkResult(answer)} respostas corretas de {perguntas.length}.</React.Fragment>
+    <React.Fragment>
+      <Slide slides={slides}></Slide>
+    </React.Fragment>
   )
-
-
-
-
 }
 
 export default App
---
+++ src/Slide.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef, useState } from 'react'
+import styles from './Slide.module.css'
+
+function Slide({ slides }) {
+    const [active, setActive] = useState(0)
+    const [position, setPosition] = useState(0)
+    const contentRef = useRef();
+
+    useEffect(() => {
+        let size = contentRef.current.getBoundingClientRect();
+        setPosition(-(size.width * active))
+    }, [active])
+
+    function slidePrev() {
+        if (active > 0)
+            setActive(active - 1)
+    }
+    function slideNext() {
+        if (active < slides.length - 1)
+            setActive(active + 1)
+    }
+    return (
+        <section className={styles.container}>
+            <nav className={styles.nav}>
+                <button onClick={slidePrev}>Anterior</button>
+                <button onClick={slideNext}>proximo</button>
+            </nav>
+            <div ref={contentRef} className={styles.content} style={{ transform: `translateX(${position}px)` }}>
+                {slides.map(slide => (
+                    <div key={slide.id} className={styles.item}>
+                        <img src={slide.picture} alt={slide.text}></img>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Slide
